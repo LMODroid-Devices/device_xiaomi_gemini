b94a3b83d10be5e17ccc4ba23be05a17edb3c916
Bruno Martins <bgcngm@gmail.com>
2021-01-03 14:39:18 +0000

    gemini: Remove inheritance of AOSP product makefiles
    This is being moved to the common device tree.
    
    Change-Id: Ie4c2e45e5c12d2b254f28854d00e137e8c28a349
    Signed-off-by: Mohammad Hasan Keramat J <ikeramat80@gmail.com>

--- lmodroid_gemini.mk
+++ lmodroid_gemini.mk
@@ -1,6 +1,6 @@
 #
 # Copyright (C) 2016 The CyanogenMod Project
-# Copyright (C) 2017-2019 The LineageOS Project
+# Copyright (C) 2017-2021 The LineageOS Project
 # Copyright (C) 2020 The LibreMobileOS Foundation
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -15,10 +15,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-
-# Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit from gemini device
 $(call inherit-product, device/xiaomi/gemini/device.mk)
